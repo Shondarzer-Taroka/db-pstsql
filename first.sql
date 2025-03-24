e193ba61be996c1a2c0e9833cf6eee79068a0744
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-03-24 11:55:15 +0600

    first commit for database postgresql

---
+++ first.sql
@@ -0,0 +1,14 @@
+-- insert into person
+-- values
+-- (104,'Elias','Cumilla'),
+-- (105,'Ramil','Dhaka'),
+-- (106,'Taslima','Gazipur')
+
+select * from person ;
+
+update person
+set name='Rolisa'
+where id=102
+
+delete from person 
+where id =101;
