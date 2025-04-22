36f96c4d73975aae9daee51070dc34849e8db30c
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-04-22 23:28:32 +0600

    add mobile number check

--- person.sql
+++ person.sql
@@ -16,3 +16,9 @@ rename column upazila to district
 alter table person
 ALTER COLUMN district
 SET  NOT NULL
+
+
+alter table person 
+add column mob varchar(15) unique check(length (mob)>=15)
+
+select * from person
